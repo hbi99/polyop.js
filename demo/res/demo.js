575ac787c06615190d4f5a1cc46ae7fa17632c68
Hakan Bilgin <hbi@longscript.com>
2017-08-30 00:12:08 +0200

    basic interaction of two polygons with vertices

--- demo/res/demo.js
+++ demo/res/demo.js
@@ -2,12 +2,16 @@
 (function(win, doc) {
 	'use strict';
 
-	var vertices = [[30, 30], [200, 30], [200, 150], [30, 150]];
+	var poly1 = [[30, 30], [300, 30], [300, 250], [30, 250]];
+	var poly2 = [[130, 130], [400, 130], [400, 350], [130, 350]];
 
 	var demo = {
 		init: function() {
 			// fast references
-			this.vertices = vertices;
+			this.vertices = {
+				poly1: poly1,
+				poly2: poly2
+			};
 			this.vertexRadius = 10;
 
 			// initate it all
@@ -22,13 +26,14 @@
 			doc.addEventListener('mousemove', this.doEvent, false);
 
 			// painter
-			this.draw.poly(vertices);
+			this.draw.poly(this.vertices);
 		},
 		doEvent: function(event) {
 			var self = demo,
 				cmd = event.type,
 				vertices = self.vertices,
-				len = vertices.length,
+				vx,
+				len,
 				radius = self.vertexRadius,
 				mx = event.pageX - self.draw.rect.left,
 				my = event.pageY - self.draw.rect.top,
@@ -40,24 +45,48 @@
 					// prevent default behaviour
 					event.preventDefault();
 				case 'mousemove':
-					if (cmd === 'mousemove' && self._selected) {
+					if (cmd === 'mousemove') {
 						ox = mx + self._clickX;
 						oy = my + self._clickY;
-						vertices[self._selected] = [ox, oy];
+						if (vertices.poly1._selected > -1) vertices.poly1[vertices.poly1._selected] = [ox, oy];
+						if (vertices.poly2._selected > -1) vertices.poly2[vertices.poly2._selected] = [ox, oy];
 					} else {
-						delete self._selected;
-						delete self._hovered;
+						// polygon 1 vertices
+						vx = vertices.poly1;
+						len = vx.length;
+						vx._selected = -1;
+						vx._hovered = -1;
 						while (len--) {
-							ox = vertices[len][0] - mx;
-							oy = vertices[len][1] - my;
+							ox = vx[len][0] - mx;
+							oy = vx[len][1] - my;
 							isVertex = Math.sqrt((ox * ox) + (oy * oy)) <= radius;
 							if (isVertex) {
 								if (cmd === 'mousedown') {
-									self._selected = len;
+									vertices.poly1._selected = len;
 									self._clickX = ox;
 									self._clickY = oy;
 								} else {
-									self._hovered = len;
+									vx._hovered = len;
+								}
+								break;
+							}
+						}
+						// polygon 2 vertices
+						vx = vertices.poly2;
+						len = vx.length;
+						vx._selected = -1;
+						vx._hovered = -1;
+						while (len--) {
+							ox = vx[len][0] - mx;
+							oy = vx[len][1] - my;
+							isVertex = Math.sqrt((ox * ox) + (oy * oy)) <= radius;
+							if (isVertex) {
+								if (cmd === 'mousedown') {
+									vertices.poly2._selected = len;
+									self._clickX = ox;
+									self._clickY = oy;
+								} else {
+									vx._hovered = len;
 								}
 								break;
 							}
@@ -67,7 +96,8 @@
 					self.draw.poly(vertices);
 					break;
 				case 'mouseup':
-					self._selected = false;
+					vertices.poly1._selected = -1;
+					vertices.poly2._selected = -1;
 					break;
 			}
 		},
@@ -79,17 +109,26 @@
 			},
 			poly: function(vertices) {
 				var ctx = this.ctx,
-					il = vertices.length,
-					i = 1;
+					il, i;
 
 				ctx.clearRect(0, 0, 1e4, 1e4);
-
 				ctx.lineWidth = 3;
 				ctx.strokeStyle = '#369';
+				
+				// polygon 1
 				ctx.beginPath();
-				ctx.moveTo(vertices[0][0], vertices[0][1]);
-				for (; i<il; i++) {
-					ctx.lineTo(vertices[i][0], vertices[i][1]);
+				ctx.moveTo(vertices.poly1[0][0], vertices.poly1[0][1]);
+				for (i=1, il=vertices.poly1.length; i<il; i++) {
+					ctx.lineTo(vertices.poly1[i][0], vertices.poly1[i][1]);
+				}
+				ctx.closePath();
+				ctx.stroke();
+
+				// polygon 2
+				ctx.beginPath();
+				ctx.moveTo(vertices.poly2[0][0], vertices.poly2[0][1]);
+				for (i=1, il=vertices.poly2.length; i<il; i++) {
+					ctx.lineTo(vertices.poly2[i][0], vertices.poly2[i][1]);
 				}
 				ctx.closePath();
 				ctx.stroke();
@@ -97,32 +136,49 @@
 				this.vertex(vertices);
 			},
 			vertex: function(vertices) {
-				var that = demo,
-					_selected = that._selected,
-					_hovered = that._hovered,
+				var _selected,
+					_hovered,
 					ctx = this.ctx,
 					radius = demo.vertexRadius / 2,
-					il = vertices.length,
-					i = 0,
-					pi2 = Math.PI * 2,
-					color;
+					il, i,
+					pi2 = Math.PI * 2;
 
-				for (; i<il; i++) {
+				// set line width
+				ctx.lineWidth = 7;
+
+				// polygon vertices 1
+				_selected = vertices.poly1._selected;
+				_hovered = vertices.poly1._hovered;
+				for (i=0, il=vertices.poly1.length; i<il; i++) {
 					ctx.beginPath();
-					ctx.lineWidth = 7;
-
 					switch (i) {
-						case _selected: color = 'rgba(100,100,200,0.65)'; break;
-						case _hovered: color = 'rgba(100,100,100,0.5)'; break;
-						default: color = 'rgba(200,100,100,0.85)';
+						case _selected: ctx.strokeStyle = 'rgba(100,100,200,0.65)'; break;
+						case _hovered: ctx.strokeStyle = 'rgba(100,100,100,0.5)'; break;
+						default: ctx.strokeStyle = 'rgba(200,100,100,0.85)';
 					}
-					ctx.strokeStyle = color;
 					ctx.fillStyle = '#fff';
 					
-					ctx.arc(vertices[i][0], vertices[i][1], radius, 0, pi2);
+					ctx.arc(vertices.poly1[i][0], vertices.poly1[i][1], radius, 0, pi2);
 					ctx.stroke();
 					ctx.fill();
-					//ctx.stroke();
+				}
+
+
+				// polygon vertices 2
+				_selected = vertices.poly2._selected;
+				_hovered = vertices.poly2._hovered;
+				for (i=0, il=vertices.poly2.length; i<il; i++) {
+					ctx.beginPath();
+					switch (i) {
+						case _selected: ctx.strokeStyle = 'rgba(100,100,200,0.65)'; break;
+						case _hovered: ctx.strokeStyle = 'rgba(100,100,100,0.5)'; break;
+						default: ctx.strokeStyle = 'rgba(200,100,100,0.85)';
+					}
+					ctx.fillStyle = '#fff';
+					
+					ctx.arc(vertices.poly2[i][0], vertices.poly2[i][1], radius, 0, pi2);
+					ctx.stroke();
+					ctx.fill();
 				}
 			}
 		}
